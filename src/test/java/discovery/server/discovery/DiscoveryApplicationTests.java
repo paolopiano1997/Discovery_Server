0c02ac70aae20b78b5fe5319dd355a8772d5c1b9
paolopiano1997 <paolopiano1997@gmail.com>
2020-11-26 22:01:14 +0100

    update Discovery

---
+++ src/test/java/discovery/server/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package discovery.server.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
